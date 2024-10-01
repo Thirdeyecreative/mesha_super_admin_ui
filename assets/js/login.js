9ed5bc223d34c38ddfa2eba92204b6b14eba834b
mYashavanth <yashavantham143@gmail.com>
2024-10-01 14:44:09 +0530

    initial commit

---
+++ assets/js/login.js
@@ -0,0 +1,67 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const passwordToggleBtn = document.getElementById("button-addon2");
+  const passwordInput = document.getElementById("password");
+  const emailInput = document.getElementById("email");
+  const loginForm = document.querySelector("form");
+  const errorDiv = document.getElementById("errorDiv");
+  const successToastEl = document.getElementById("successToast");
+  const toast = new bootstrap.Toast(successToastEl);
+
+  // Toggle password visibility
+  passwordToggleBtn.addEventListener("click", function () {
+    if (passwordInput.type === "password") {
+      passwordInput.type = "text";
+      passwordToggleBtn.innerHTML = '<i class="bi bi-eye-slash"></i>';
+    } else {
+      passwordInput.type = "password";
+      passwordToggleBtn.innerHTML = '<i class="bi bi-eye"></i>';
+    }
+  });
+
+  // Clear error message when focusing on inputs
+  emailInput.addEventListener("change", () => {
+    errorDiv.textContent = "";
+  });
+
+  passwordInput.addEventListener("change", () => {
+    errorDiv.textContent = "";
+  });
+
+  // Handle form submission
+  loginForm.addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    // Clear previous error messages
+    errorDiv.textContent = "";
+
+    // Create a new FormData object from the form
+    const formData = new FormData(loginForm);
+
+    // Make an API call to the login endpoint
+    fetch("https://stingray-app-4smpo.ondigitalocean.app/super-admin-users", {
+      method: "POST",
+      body: formData, // Use FormData directly
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.errFlag === 0) {
+          // Successful login, store the token in localStorage
+          localStorage.setItem("authToken", data.token);
+          console.log("Token:", data.token);
+          
+          // Show Bootstrap toast for successful login
+          toast.show();
+          window.location.href = "mesha_customer_management.html";
+        } else {
+          console.log("Error:", data);
+          errorDiv.textContent = data.message + " Please try again.";
+          emailInput.focus();
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        // General error handling (network issues, etc.)
+        errorDiv.textContent = "An error occurred, please try again.";
+      });
+  });
+});
---
+++ assets/js/navbar.js
@@ -0,0 +1,158 @@
+function loadNavBar() {
+  const navContent = `
+    <figure>
+      <img src="./assets/images/index/logo.png" alt="logo" />
+    </figure>
+    <div class="navContent">
+      <div class="navItems" id="dashboard_link">
+        <i class="bi bi-house-door" style="font-size: 20px"></i>
+        <p>Dashboard</p>
+      </div>
+      <div class="navItems" id="mesha_customer_management_link">
+        <i class="bi bi-person-workspace" style="font-size: 20px"></i>
+        <p>Mesha Customer Management</p>
+      </div>
+      <div class="accordion" id="accordionExample">
+        <div class="accordion-item">
+          <h2 class="accordion-header">
+            <button
+              class="accordion-button collapsed"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#flush-collapseOne"
+              aria-expanded="false"
+              aria-controls="flush-collapseOne"
+            >
+              <i class="bi bi-ev-front" style="font-size: 20px"></i>
+              <p>Device Management</p>
+            </button>
+          </h2>
+          <div
+            id="flush-collapseOne"
+            class="accordion-collapse collapse"
+            data-bs-parent="#accordionExample"
+          >
+            <div class="navItems" id="device_management-link">
+              <i
+                class="bi bi-house-door"
+                style="font-size: 20px; color: transparent"
+              ></i>
+              <p>Device Management</p>
+            </div>
+            <div class="navItems" id="customer_management_link">
+              <i 
+                class="bi bi-people" 
+                style="font-size: 20px; color: transparent"
+              ></i>
+              <p>Device Parameters</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="navItems" id="user_role_management_link">
+        <i class="bi bi-people" style="font-size: 20px;" ></i>
+        <p>User Role Management</p>
+      </div>
+      <div class="navItems" id="super_user_role_management_link">
+        <i class="bi bi-people" style="font-size: 20px;" ></i>
+        <p>Super User Role Management</p>
+      </div>
+      <div class="navItems" id="map-view-link">
+        <i class="bi bi-pin-map" style="font-size: 20px"></i>
+        <p>Map View</p>
+      </div>
+      <div class="navItems" id="settings-link">
+        <i class="bi bi-gear" style="font-size: 20px"></i>
+        <p>Settings</p>
+      </div>
+      <div class="navItems" id="profile-link">
+        <i class="bi bi-person" style="font-size: 20px"></i>
+        <p>Profile</p>
+      </div>
+      <div class="navItems" id="faqs-link">
+        <i class="bi bi-question-octagon" style="font-size: 20px"></i>
+        <p>FAQs</p>
+      </div>
+      <div class="navItems" id="logout-link">
+        <i class="bi bi-power" style="font-size: 20px"></i>
+        <p>Logout</p>
+      </div>
+    </div>
+  `;
+
+  document.getElementById("nav").innerHTML = navContent;
+
+  setActiveLink();
+
+  document.getElementById("logout-link").addEventListener("click", function () {
+    localStorage.removeItem("authToken");
+    window.location.href = "/login.html";
+  });
+  document
+    .getElementById("dashboard_link")
+    .addEventListener("click", function () {
+      window.location.href = "/index.html";
+    });
+  document
+    .getElementById("mesha_customer_management_link")
+    .addEventListener("click", function () {
+      window.location.href = "/mesha_customer_management.html";
+    });
+  document
+    .getElementById("device_management-link")
+    .addEventListener("click", function () {
+      window.location.href = "/device_management.html";
+    });
+  document
+    .getElementById("user_role_management_link")
+    .addEventListener("click", function () {
+      window.location.href = "/user_role_management.html";
+    });
+  document
+    .getElementById("super_user_role_management_link")
+    .addEventListener("click", function () {
+      window.location.href = "/super_user_role_management.html";
+    });
+}
+
+function setActiveLink() {
+  const path = window.location.pathname;
+
+  const links = {
+    "/": "dashboard_link",
+    "/index.html": "dashboard_link",
+    "/mesha_customer_management.html": "mesha_customer_management_link",
+    "/customer-management": "customer_management_link",
+    "/device_management.html": "device_management-link",
+    "/user_role_management.html": "user_role_management_link",
+    "/super_user_role_management.html": "super_user_role_management_link",
+    "/map-view": "map-view-link",
+    "/settings": "settings-link",
+    "/profile": "profile-link",
+    "/faqs": "faqs-link",
+  };
+
+  const navItems = document.querySelectorAll(".navItems");
+  navItems.forEach((item) => {
+    item.classList.remove("active");
+  });
+
+  if (links[path]) {
+    document.getElementById(links[path]).classList.add("active");
+  }
+
+  // Check if any of the accordion links are active and open the accordion if needed
+  const accordionLinks = ["/device_management.html", "/device_parameters.html"];
+  const isAccordionActive = accordionLinks.includes(path);
+
+  if (isAccordionActive) {
+    const accordionButton = document.querySelector(".accordion-button");
+    accordionButton.classList.remove("collapsed");
+    accordionButton.setAttribute("aria-expanded", "true");
+
+    const accordionCollapse = document.getElementById("flush-collapseOne");
+    accordionCollapse.classList.add("show");
+  }
+}
+
+document.addEventListener("DOMContentLoaded", loadNavBar);
